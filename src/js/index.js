dfacdc24c5b9c973d3e1d225a24b6c718f626861
Alen <alen.rajher@gmail.com>
2020-03-03 12:19:05 +0100

    napravio recipeView i izgleda super, al sam se bome i napatio...

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
+
 import { elements, renderLoader, clearLoader } from "./views/base";
 /*GLOBAL STATE OF THE APP 
 - Search object
@@ -10,9 +13,11 @@ import { elements, renderLoader, clearLoader } from "./views/base";
 
 const state = {};
 
+////SEARCH CONTROLER
 const controlSearch = async () => {
   //1. Get query from view
   const query = searchView.getInput();
+
   console.log(query);
   if (query) {
     //2. New search object and add to state
@@ -22,12 +27,17 @@ const controlSearch = async () => {
     searchView.clearInput();
     searchView.clearResults();
     renderLoader(elements.searchRes);
-    //4. Search for recipes
-    await state.search.getResults();
+    try {
+      //4. Search for recipes
+      await state.search.getResults();
 
-    //5. Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+      //5. Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      alert("Something wrong with the search...");
+      clearLoader();
+    }
   }
 };
 
@@ -35,3 +45,54 @@ elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+    console.log(goToPage);
+  }
+});
+
+////RECIPE CONTROLER
+
+const controlRecipe = async () => {
+  //Get ID from url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    //Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    //create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      //get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      console.log(state.recipe.ingredients);
+      state.recipe.parseIngredients();
+
+      //calulate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      //render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (err) {
+      alert("Error processing recipe!");
+    }
+  }
+};
+
+/////////DVA NACINA
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+["hashchange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,13 +2,14 @@ export const elements = {
   searchForm: document.querySelector(".search"),
   searchInput: document.querySelector(".search__field"),
   searchRes: document.querySelector(".results"),
-  searchResList: document.querySelector(".results__list")
+  searchResList: document.querySelector(".results__list"),
+  searchResPages: document.querySelector(".results__pages"),
+  recipe: document.querySelector(".recipe")
 };
 
 export const elementStrings = {
   loader: "loader"
 };
-
 export const renderLoader = parent => {
   const loader = `
         <div class="${elementStrings.loader}">
@@ -22,5 +23,7 @@ export const renderLoader = parent => {
 
 export const clearLoader = () => {
   const loader = document.querySelector(`.${elementStrings.loader}`);
-  if (loader) loader.parentElement.removeChild(loader);
+  if (loader) {
+    loader.parentElement.removeChild(loader);
+  }
 };
